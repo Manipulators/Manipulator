17bb0754dd2d6148519f3bc47fb20993aa7bcaa1
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2014-02-04 11:40:36 +0100

    Add file menu

--- manipulator/main.cpp
+++ manipulator/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication qApplication(argc, argv);
 
-    // Create a window
+    // Create the main window
     MainWindow main_window;
     main_window.show();
 
