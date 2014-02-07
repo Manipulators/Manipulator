fb5ef95886698d96efbb355b83f370dc06af7d64
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2014-02-07 18:01:15 +0100

    Indentation of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -22,61 +22,65 @@ typedef CGAL::Polygon_2<Kernel>                     Polygon_2;
 
 
 
-int main (int argc, char **argv) {
-
-   QApplication app(argc, argv);
-
-  QGraphicsScene scene;
-  std::string line;
-  std::ifstream myfile ("../data/obstacles.data");
-  char * pch;
-  float x,y;
-  int i=0;
-  Polygon_2 obstacles [500];
-  if (myfile.is_open())
-  {
-    getline (myfile,line);//Read size
+int main (int argc, char **argv)
+{
+    QApplication app(argc, argv);
     
-    pch = NULL;
-    char * cstr = new char [line.length()+1];
-    strcpy (cstr, line.c_str());
-    pch = strtok (cstr," ");
-    x = atof( pch );
-    pch = strtok (NULL, " ;");
-    y = atof( pch );
-    scene.setSceneRect(0,0, x, y);
-    while ( getline (myfile,line) )
-    { //Read a polygon
-	QPolygonF Pqt;//QT
-	Polygon_2   P;//CGAL
-	pch = NULL;
-	char * cstr = new char [line.length()+1];
-  	strcpy (cstr, line.c_str());
-	pch = strtok (cstr," ;");
-  	while (pch != NULL)
-  	{
-	 x = atof( pch );
-    	 pch = strtok (NULL, " ;");
-	 y = atof( pch );
-	 pch = strtok (NULL, " ;");
-	 P.push_back (Point_2 (x, y));
-	 Pqt << QPointF(x,y);
-  	} 
-	obstacles[i] = P; i++; scene.addPolygon(Pqt,QPen(Qt::black),QBrush(Qt::darkBlue));
+    QGraphicsScene scene;
+    std::string line;
+    std::ifstream myfile("../data/obstacles.data");
+    char *pch;
+    float x, y;
+    int i = 0;
+    Polygon_2 obstacles[500];
+    if (myfile.is_open())
+    {
+        getline (myfile, line); //Read size
+        
+        pch = NULL;
+        char *cstr = new char[line.length() + 1];
+        strcpy(cstr, line.c_str());
+        pch = strtok(cstr, " ");
+        x = atof(pch);
+        pch = strtok(NULL, " ;");
+        y = atof(pch);
+        scene.setSceneRect(0, 0, x, y);
+        // Read a polygon
+        while (getline(myfile, line))
+        {
+            // Qt
+	        QPolygonF Pqt;
+            // CGAL
+	        Polygon_2 P;
+	        pch = NULL;
+	        char *cstr = new char[line.length() + 1];
+  	        strcpy (cstr, line.c_str());
+	        pch = strtok(cstr, " ;");
+  	        while (pch != NULL)
+  	        {
+	            x = atof(pch);
+    	        pch = strtok(NULL, " ;");
+	            y = atof(pch);
+	            pch = strtok(NULL, " ;");
+	            P.push_back(Point_2 (x, y));
+	            Pqt << QPointF(x, y);
+  	        } 
+	        obstacles[i] = P;
+            i++;
+            scene.addPolygon(Pqt, QPen(Qt::black), QBrush(Qt::darkBlue));
+        }
+        myfile.close();
     }
-    myfile.close();
-  }
-
-  else std::cout << "Unable to open file"; 
-   QGraphicsView* view = new QGraphicsView(&scene);
+    else std::cout << "Unable to open file\n"; 
+    
+    QGraphicsView* view = new QGraphicsView(&scene);
     CGAL::Qt::GraphicsViewNavigation navigation;
     view->installEventFilter(&navigation);
     view->viewport()->installEventFilter(&navigation);
     view->setRenderHint(QPainter::Antialiasing);
-
     view->show();
-
-  return app.exec();
+    
+    return app.exec();
 }
 
 
