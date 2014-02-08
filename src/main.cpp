a69a92095e402c6fb05b1290c94e44bf82f4c391
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2014-02-09 00:41:48 +0100

    Clean data and remove CGAL/Qt test

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,10 @@
-#include <CGAL/MP_Float.h>
-#include <CGAL/Quotient.h>
-
-typedef CGAL::Quotient<CGAL::MP_Float>                Number_type;
-
-#include <CGAL/Cartesian.h>
-#include <CGAL/minkowski_sum_2.h>
-
-typedef CGAL::Cartesian<Number_type>                Kernel;
-typedef Kernel::Point_2                             Point_2;
-typedef CGAL::Polygon_2<Kernel>                     Polygon_2;
-
-#include <QtGui>
-
 #include <QApplication>
-#include <QWidget>
 #include "mainwindow.h"
 
 
 int main (int argc, char **argv)
 {
     QApplication app(argc, argv);
-    
-    QGraphicsScene scene;
-    std::string line;
-    std::ifstream myfile("../data/obstacles.data");
-    char *pch;
-    float x;
-    float y;
-    int i = 0;
-    Polygon_2 obstacles[500];
-    
-    if (myfile.is_open())
-    {
-        getline (myfile, line); //Read size
-        
-        pch = NULL;
-        char *cstr = new char[line.length() + 1];
-        strcpy(cstr, line.c_str());
-        pch = strtok(cstr, " ");
-        x = atof(pch);
-        pch = strtok(NULL, " ;");
-        y = atof(pch);
-        scene.setSceneRect(0, 0, x, y);
-        // Read a polygon
-        while (getline(myfile, line))
-        {
-            // Qt
-	        QPolygonF Pqt;
-            // CGAL
-	        Polygon_2 P;
-	        pch = NULL;
-	        char *cstr = new char[line.length() + 1];
-  	        strcpy (cstr, line.c_str());
-	        pch = strtok(cstr, " ;");
-  	        while (pch != NULL)
-  	        {
-	            x = atof(pch);
-    	        pch = strtok(NULL, " ;");
-	            y = atof(pch);
-	            pch = strtok(NULL, " ;");
-	            P.push_back(Point_2 (x, y));
-	            Pqt << QPointF(x, y);
-  	        } 
-	        obstacles[i] = P;
-            i++;
-            scene.addPolygon(Pqt, QPen(Qt::black), QBrush(Qt::darkBlue));
-        }
-        myfile.close();
-    }
-    else std::cout << "Unable to open file\n"; 
-    
-    QGraphicsView *view = new QGraphicsView(&scene);
-    //view->show();
     
     MainWindow window;
     window.show();
