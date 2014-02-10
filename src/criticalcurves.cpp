a1f100eb312fdc913909a8d9b3d6f16d6e5de37d
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2014-02-11 00:58:06 +0100

    Add criticalcurves files in the build process

--- src/criticalcurves.cpp
+++ src/criticalcurves.cpp
@@ -1,6 +1,11 @@
 #include "criticalcurves.h"
 
+
+/* TODO: avoid build error.
+
 Polygon offset (Polygon poly, double lOffset)
 {
-  return **(CGAL::create_offset_polygons_2<Polygon>(-lOffset,*(CGAL::create_interior_straight_skeleton_2(poly)))).begin();
+    return **(CGAL::create_offset_polygons_2<Polygon>(-lOffset,*(CGAL::create_interior_straight_skeleton_2(poly)))).begin();
 }
+
+*/
--- src/criticalcurves.h
+++ src/criticalcurves.h
@@ -5,9 +5,14 @@
 #include<CGAL/create_offset_polygons_2.h>
 #include <CGAL/Polygon_2.h>
 
+
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K ;
 typedef CGAL::Polygon_2<K>           Polygon ;
 
+/* TODO: avoid build error.
+
 Polygon offset (Polygon, double);
+
+*/
 
 #endif
