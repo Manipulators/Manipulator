a5d9a1fa686bcbe19333eb3cfd0ea560d5a09e49
ChrisENS <corderochristophe@gmail.com>
2014-02-13 13:43:23 +0100

    Add vertices

--- src/graph.cpp
+++ src/graph.cpp
@@ -4,3 +4,32 @@ Graph::Graph()
 {
   n = 0;
 }
+
+int Graph::index(double xi,double yi)
+{
+    int i;
+    for (i = 0; i < (this->n); i++)
+    {
+        Node m = (this->nodes)[i];
+        if (m.x == xi && m.y == yi) {return i;};
+    }
+    //Add node
+    Node node;
+    node.x = xi;
+    node.y = yi;
+    (this->nodes)[this->n] = node;
+    this->n = (this->n) + 1;
+    return (this->n) - 1;
+}
+
+void Graph::addVertice(double x1,double y1,double x2,double y2,int isarc, double xc =-1,double yc=-1)
+{
+    int index1 = index(x1,y1);
+    int index2 = index(x2,y2);
+    Vertice v;
+    v.isarc = isarc;
+    v.xc = xc;
+    v.yc = yc;
+    this->matrix[index1][index2] = v;
+    this->matrix[index2][index1] = v;
+}
--- src/graph.h
+++ src/graph.h
@@ -1,6 +1,5 @@
 #ifndef GRAPH_H
 #define GRAPH_H
-
 #include <list>
 
 struct Node
@@ -11,19 +10,26 @@ struct Node
 
 struct Vertice
 {
-    Node node;
     int isarc;
     double xc;
     double yc;
+    //Original wall or corner (cf. Labeling)
+    /*double x1;
+    double y1;
+    double x2;
+    double y2;*/
 };
 
 class Graph
 {
 private:
-    std::list<Vertice> graphe [1000];
+    Vertice matrix [1000][1000];
+    Node nodes [1000];
 public:
     int n;
     Graph();
+    void addVertice(double,double,double,double,int, double,double);
+    int index(double,double);
 };
 
 
