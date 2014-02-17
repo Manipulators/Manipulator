e689f48e603c189131b74e34d95260bd19bf86b6
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2014-02-17 21:13:58 +0100

    Add missing include

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include "mainwindow.h"
 #include "barriers.h"
-#include "robot.h"
-#include "movableobject.h"
-#include "obstacles.h"
+#include "bodie.h"
 #include "criticalcurves.h"
 #include <QApplication>
 #include <iostream>
